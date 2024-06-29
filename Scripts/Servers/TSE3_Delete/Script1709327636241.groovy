7fc10fbd8404dce96c42d0099b3d2daca41cfb8b
Tarikelhaiti <tarik.elhaiti1996@gmail.com>
2024-06-29 14:06:48 +0100

    fix server test

--- Scripts/Servers/TSE3_Delete/Script1709327636241.groovy
+++ Scripts/Servers/TSE3_Delete/Script1709327636241.groovy
@@ -1,29 +1,25 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.By as By
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import org.openqa.selenium.support.ui.ExpectedConditions as ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait as WebDriverWait
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
 
 WebUI.callTestCase(findTestCase('Login/Pretest - Admin Login'), [('variable') : ''], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.click(findTestObject('Object Repository/servers/Page_Servers - PowerFolder/lang_Servers'))
 
-WebUI.click(findTestObject('servers/Page_Servers - PowerFolder/td_server'))
+WebDriverWait wait = new WebDriverWait(DriverFactory.getWebDriver(), 5)
 
-WebUI.verifyElementVisible(findTestObject('Object Repository/servers/Page_Servers - PowerFolder/span_server'), FailureHandling.STOP_ON_FAILURE)
+// Attendre que l'élément devienne visible
+WebElement settings_button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath('//td[1]/span')))
+
+// Cliquer sur l'élément
+settings_button.click()
 
 WebUI.click(findTestObject('Object Repository/servers/Page_Servers - PowerFolder/span_Restart_pica-table-selection-multi pic_96fda4'))
 
@@ -31,7 +27,14 @@ WebUI.click(findTestObject('Object Repository/servers/Page_Servers - PowerFolder
 
 WebUI.delay(2)
 
-WebUI.verifyElementVisible(findTestObject('servers/Page_Servers - PowerFolder/td_server'), FailureHandling.STOP_ON_FAILURE)
+// Vérifier que l'élément est présent à la fin du test
+try {
+    WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath('//td[1]/span')))
+    assert element != null : 'L\'élément //td[1]/span n\'est pas présent à la fin du test.'
+    println('Le serveur est toujours présent.')
+} catch (Exception e) {
+    WebUI.takeScreenshot()
+    throw new AssertionError('Le serveur est supprimé.')
+} 
 
 WebUI.closeBrowser()
-
